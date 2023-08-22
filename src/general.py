9af2a916a898013cbe7ec796639e4e0af431f605
cksajil <sajilck@gmail.com>
2023-08-22 20:32:30 +0530

    Added tokenizer download code

--- src/general.py
+++ src/general.py
@@ -26,7 +26,7 @@ def create_models_folder():
 
 def download_model():
     """
-    Download pretrained model if not already downloaded
+    Download pretrained models if not already downloaded
     """
     if not os.path.exists(os.path.join(config["model_loc"], config["model_name"])):
         print("Downloading pretrained Attention dot model if not exists")
@@ -34,7 +34,8 @@ def download_model():
         response = requests.get(f"https://zenodo.org/api/records/{doi}")
         data = response.json()
         files = data["files"]
-        model_files = [file for file in files if file["key"].endswith(".h5")]
+        formats = [".hdf5", ".pickle"]
+        model_files = [file for file in files if file["key"].endswith(formats)]
 
         if len(model_files) == 0:
             print("No model files found.")
@@ -45,9 +46,7 @@ def download_model():
 
             response = requests.get(file_url, stream=True)
             segments = response.iter_content()
-            with open(
-                os.path.join(config["model_loc"], config["model_name"]), "wb"
-            ) as file:
+            with open(os.path.join(config["model_loc"], model_filename), "wb") as file:
                 for chunk in tqdm(segments):
                     file.write(chunk)
 
