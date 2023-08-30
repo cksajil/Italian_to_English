0cb273334cc79231e31614ca770b2f07d5c774b0
cksajil <sajilck@gmail.com>
2023-08-30 10:23:32 +0530

    Update seq2seq model

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,4 @@
 from .general import create_models_folder, download_model
 from .general import load_config
 from .seq2seq import predict
+from models import encoder_decoder
--- src/seq2seq.py
+++ src/seq2seq.py
@@ -1,33 +1,14 @@
 import pickle
-import tensorflow as tf
 import numpy as np
+import tensorflow as tf
 from os.path import join
+from utils import load_pretrained_model
 from src import load_config
-from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.losses import SparseCategoricalCrossentropy
+from src import encoder_decoder
 
 
 config = load_config("config.yaml")
-loss_object = SparseCategoricalCrossentropy(from_logits=True, reduction="none")
-
-
-def custom_loss(real, pred):
-    mask_loss = tf.math.logical_not(tf.math.equal(real, 0))
-    loss_metric = loss_object(real, pred)
-
-    mask_loss = tf.cast(mask_loss, dtype=loss_metric.dtype)
-    loss_metric *= mask_loss
-    return tf.reduce_mean(loss_metric)
-
-
-def load_pretrained_model():
-    """
-    Function to load pretrained attention model and return it
-    """
-    file_path = join(config["model_loc"], config["model_name"])
-    model = load_model(file_path, custom_objects={"loss_function": custom_loss})
-    return model
 
 
 def load_tokenizer(language="italian"):
@@ -99,3 +80,33 @@ def predict(text_input):
         curr_vec = np.reshape(curr_vec, (1, 1))
 
     return " ".join(prediction_string), attention_plot
+
+
+input_len = 20
+output_len = 20
+enc_units = 256
+att_units = 256
+dec_units = 256
+embedding_size = 100
+vocab_size_eng = 13416
+vocab_size_ita = 27029
+BATCH_SIZE = 512
+
+tf.keras.backend.clear_session()
+
+score_function = "dot"
+
+model_2_dot = encoder_decoder(
+    encoder_inputs_length=input_len,
+    decoder_inputs_length=output_len,
+    output_vocab_size=vocab_size_eng,
+    encoding_lang_vocab_size=vocab_size_ita + 1,
+    decoding_lang_vocab_size=vocab_size_eng + 1,
+    enc_embedding_size=embedding_size,
+    dec_embedding_size=embedding_size,
+    encoder_lstm_unit=enc_units,
+    decoder_lstm_unit=dec_units,
+    batch_size=BATCH_SIZE,
+    score_fun=score_function,
+    attn_units=att_units,
+)
